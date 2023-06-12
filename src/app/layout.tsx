73510078cc497373ca90babda9e2ccb5ac7784fc
Glendson <gztomazetto@gmail.com>
2023-06-12 18:48:44 +0100

    aula 04

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as Jamjuree,
 } from 'next/font/google'
+import { Hero } from '@/components/Hero'
+import { Profile } from '@/components/Profile'
+import { Signin } from '@/components/Signin'
+import { Copyright } from '@/components/Copyright'
+import { cookies } from 'next/headers'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 const jamjuree = Jamjuree({
@@ -14,14 +19,39 @@ const jamjuree = Jamjuree({
 
 export const metadata = {
   title: 'NLW Spacetime',
-  description: 'Uma capsula do tempo contruida com React, NextJS, TailwindCSS e Typescript',
+  description:
+    'Uma capsula do tempo contruida com React, NextJS, TailwindCSS e Typescript',
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <html lang="en">
-      <body className={`${roboto.variable} ${jamjuree.variable} font-sans bg-gray-900 text-gray-100`}>
-        {children}
+      <body
+        className={`${roboto.variable} ${jamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        <main className="grid min-h-screen grid-cols-2">
+          {/* left */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {/* blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+            {/* stripes */}
+            <div className="absolute bottom-0  right-2 top-0 w-2 bg-stripes" />
+
+            {isAuthenticated ? <Profile /> : <Signin />}
+
+            <Hero />
+
+            <Copyright />
+          </div>
+
+          {/* right */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16 ">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
